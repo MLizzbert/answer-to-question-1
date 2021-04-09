c978b4b8ee6822a1912869dfdeeec3168488011d
unknown <mlizzbert@gmail.com>
2021-04-09 16:42:04 +0100

    first commit

---
+++ answer1.js
@@ -0,0 +1,10 @@
+let arrayList = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 0, 11], [12, 13, 14, 15]];
+
+
+const myFunct = () => {
+    for (let arrayLst of arrayList) {
+        console.log(arrayLst);
+    }
+};
+myFunct();
+
